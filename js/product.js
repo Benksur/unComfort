e31dfca10ed178d7f55b042b4b045f95695810f6
Benksur <tyjbehnke1@gmail.com>
2025-05-18 13:32:11 +1000

    edited styling, added drop down

--- js/product.js
+++ js/product.js
@@ -13,16 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 <div class="product-information-container">
                     <div class="product-code-name">
                         <a class="product-page-title">${product.code} ${product.name}</a>
-                        <a class="product-materials">${product.materials}</a>
+                        <a class="product-about">${product.materials}</a>
+                        <a class="product-about">manufactured in australia</a>
                     </div>
-                    <div class="product-price">$${product.price}</div>
                 </div>
-                <div class="product-card">
-                    <img src="../../${product.imageUrl}" alt="${product.name}">
-                    <img src="../../img/${product.code}_1.jpg" alt="${product.name}">
-                    <img src="../../img/${product.code}_2.jpg" alt="${product.name}">
-                    <img src="../../img/${product.code}_3.jpg" alt="${product.name}">
-                    <img src="../../img/${product.code}_4.jpg" alt="${product.name}">
+                <div class="product-image-container">
+                    <div class="product-card">
+                        <img src="../../${product.imageUrl}" alt="${product.name}">
+                        <img src="../../img/${product.code}_1.jpg" alt="${product.name}">
+                        <img src="../../img/${product.code}_2.jpg" alt="${product.name}">
+                        <img src="../../img/${product.code}_3.jpg" alt="${product.name}">
+                        <img src="../../img/${product.code}_4.jpg" alt="${product.name}">
+                    </div>
+                </div>
+                <div class="product-add-container">
+                    <select class="size-select">
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                    </select>
                 </div>
             </div>
         `;
