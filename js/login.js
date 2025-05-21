cee1e4c0346ac03cf952b61431a5f717318c52d5
Benksur <tyjbehnke1@gmail.com>
2025-05-21 21:59:07 +1000

    finished policies, added arrow

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function login(){
+    alert("this will trigger shopify api credentials. too lazy to implement lol")
+}
+
+function signUp(){
+    alert("this will trigger shopify api credentials. too lazy to implement lol")
+}
